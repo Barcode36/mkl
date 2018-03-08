29b95bba8b1080761da9e1b3bba517bf60cf5101
jkero <jkero@jkero.com>
2018-03-08 16:14:57 +0100

    Szkielet

---
+++ MKL_Shop/src/mkl_shop/manager/FXMLManagerController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mkl_shop.manager;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author jkero
+ */
+public class FXMLManagerController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
