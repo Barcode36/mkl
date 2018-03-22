7a8270846b38c739ded538ae41eb0df2ba030dd4
michal <michal@o2.pl>
2018-03-22 19:04:18 +0100

    layout placowek

---
+++ MKL_Shop/mkl_database_bezrelacji.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 21 Mar 2018, 19:51
+-- Wersja serwera: 10.1.31-MariaDB
+-- Wersja PHP: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `mkl_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `role`
+--
+-- Błąd odczytu struktury tabeli mkl_database.role: #1146 - Table 'mkl_database.role' doesn't exist
+-- Błąd odczytu danych dla tabeli mkl_database.role: #1064 - Something is wrong in your syntax obok 'FROM `mkl_database`.`role`' w linii 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- MKL_Shop/src/mkl_shop/manager/placowki/FXMLSzczegolyController.java
+++ MKL_Shop/src/mkl_shop/manager/placowki/FXMLSzczegolyController.java
@@ -5,9 +5,14 @@
  */
 package mkl_shop.manager.placowki;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +21,73 @@ import javafx.fxml.Initializable;
  */
 public class FXMLSzczegolyController implements Initializable {
 
+    @FXML
+    private JFXButton Button_Glowna;
+    @FXML
+    private JFXButton Button_Pracownicy;
+    @FXML
+    private JFXButton Button_Produkty;
+    @FXML
+    private JFXButton Button_Sprzedaz;
+    @FXML
+    private JFXButton Button_Wstecz;
+    @FXML
+    private Pane Pane_Glowna;
+    @FXML
+    private Pane Pane_Pracownicy;
+    @FXML
+    private Pane Pane_Produkty;
+    @FXML
+    private Pane Pane_Sprzedaz;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        Pane_Glowna.setVisible(true);
+        Pane_Pracownicy.setVisible(false);
+        Pane_Produkty.setVisible(false);
+        Pane_Sprzedaz.setVisible(false);
+    }
+
+    @FXML
+    private void goGlowna(ActionEvent event) {
+        Pane_Glowna.setVisible(true);
+        Pane_Pracownicy.setVisible(false);
+        Pane_Produkty.setVisible(false);
+        Pane_Sprzedaz.setVisible(false);
+
+    }
+
+    @FXML
+    private void goPracownicy(ActionEvent event) {
+        Pane_Glowna.setVisible(false);
+        Pane_Pracownicy.setVisible(true);
+        Pane_Produkty.setVisible(false);
+        Pane_Sprzedaz.setVisible(false);
+    }
+
+    @FXML
+    private void goProdukty(ActionEvent event) {
+        Pane_Glowna.setVisible(false);
+        Pane_Pracownicy.setVisible(false);
+        Pane_Produkty.setVisible(true);
+        Pane_Sprzedaz.setVisible(false);
+    }
+
+    @FXML
+    private void goSprzedaz(ActionEvent event) {
+        Pane_Glowna.setVisible(false);
+        Pane_Pracownicy.setVisible(false);
+        Pane_Produkty.setVisible(false);
+        Pane_Sprzedaz.setVisible(true);
+    }
+
+    @FXML
+    private void wstecz(ActionEvent event) {
+        Stage stage = (Stage) Button_Wstecz.getScene().getWindow();
+        stage.close();
+    }
+
 }
