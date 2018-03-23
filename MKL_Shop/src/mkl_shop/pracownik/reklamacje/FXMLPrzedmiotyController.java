a9708bcf163bacb2884970efa0ffc1786263efb3
BlackHawk <BlackHawk@asus>
2018-03-23 15:18:25 +0100

    nowa reklamacja

---
+++ MKL_Shop/src/mkl_shop/pracownik/reklamacje/FXMLPrzedmiotyController.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mkl_shop.pracownik.reklamacje;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author BlackHawk
+ */
+public class FXMLPrzedmiotyController implements Initializable {
+
+    @FXML
+    private JFXListView<?> lvPrzedmioty;
+    @FXML
+    private JFXButton bWyjscie;
+    @FXML
+    private JFXTextField tfWyszukaj;
+    @FXML
+    private JFXButton bWybierz;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
+        
+        FilteredList<Klient> filteredKlient = new FilteredList <>(listView.getItems(), e->true);
+        txSzukaj.setOnKeyReleased(e->{
+            txSzukaj.textProperty().addListener((observableValue, oldValue, newValue) ->{
+                filteredKlient.setPredicate((Predicate<? super Klient>) k->{
+                    if (newValue==null || newValue.isEmpty()){
+                        return true;
+                    }
+                    String lcFilter = newValue.toLowerCase();
+                    if (k.getImie().toLowerCase().contains(lcFilter) || k.getNazwisko().toLowerCase().contains(lcFilter) || k.getNrKarty().toLowerCase().contains(lcFilter)){
+                        return true;
+                    }
+                    return false;
+                });
+                });
+            SortedList<Klient> sortedKlient = new SortedList<>(filteredKlient);            
+            listView.setItems(sortedKlient);            
+        });
+        
+        */
+        
+        
+        
+    }    
+
+
+    @FXML
+    private void zamknijOkno(ActionEvent event) {
+        Stage stage = (Stage) bWyjscie.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void wybierz(ActionEvent event) {
+    }
+    
+}
