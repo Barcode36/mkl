fe7e0e99ffa17c50e2740c8dc70ebed545352952
Ukasz <mrlukas_95@o2.pl>
2018-03-24 16:07:41 +0100

    admin/1

--- MKL_Shop/src/mkl_shop/admin/FXMLAdminController.java
+++ MKL_Shop/src/mkl_shop/admin/FXMLAdminController.java
@@ -5,16 +5,30 @@
  */
 package mkl_shop.admin;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
  * FXML Controller class
  *
- * @author jkero
+ * @author Łukasz
  */
 public class FXMLAdminController implements Initializable {
+
+    @FXML
+    private JFXButton btnPlacowki;
+    @FXML
+    private JFXButton btnWiadomosci;
+    @FXML
+    private JFXButton btnHasla;
+    @FXML
+    private JFXButton btnWyjscie;
+    @FXML
+    private JFXButton btnPracownicy;
 
     /**
      * Initializes the controller class.
@@ -23,5 +37,24 @@ public class FXMLAdminController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+
+
+    @FXML
+    private void Wyjscie(ActionEvent event) {
+    }
+
+
+    @FXML
+    private void zarzadzajPlacowki(ActionEvent event) {
+    }
+
+    @FXML
+    private void zarzadzajWiadomosci(ActionEvent event) {
+    }
+
+    @FXML
+    private void zarzadzajHasla(ActionEvent event) {
+    }
     
 }
