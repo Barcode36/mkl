1444e8287801621c7a2824ecc6b2670d6c1a01d4
BlackHawk <BlackHawk@asus>
2018-06-02 16:52:51 +0200

    klient usuwanie

--- MKL_Shop/src/mkl_shop/pracownik/klient/FXMLKlientController.java
+++ MKL_Shop/src/mkl_shop/pracownik/klient/FXMLKlientController.java
@@ -14,6 +14,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 import java.util.function.Predicate;
 import java.util.logging.Level;
@@ -34,6 +35,7 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import mkl_shop.alert.AlertMaker;
 import mkl_shop.connection.DBConnection;
 import mkl_shop.pracownik.modele.Klient;
 
@@ -61,80 +63,62 @@ public class FXMLKlientController implements Initializable {
     @FXML
     private AnchorPane apMain;
 
-    
     public static Integer idKlienta;
     //private ObservableList <Klient> dataKlienci;
-    
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
         bWyjscie.setFocusTraversable(false);
         bNowyKlient.setFocusTraversable(false);
         tfWyszukaj.setFocusTraversable(false);
-        
+
         //dataKlienci = FXCollections.observableArrayList();
-        
         Connection conn = DBConnection.Connect();
-        
-        
+
         try {
             Statement ps = conn.createStatement();
             ResultSet rs = ps.executeQuery("SELECT id_klienta, imie_klienta, nazwisko_klienta, kod_pocztowy_klienta, "
                     + "miejscowosc_klienta, adres_klienta, telefon_klienta, id_klienta as numer_karty, liczba_punktow "
                     + "FROM klient;");
-            
-            
-            while (rs.next()){
+
+            while (rs.next()) {
                 lvKlienci.getItems().add(new Klient(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getInt(8), rs.getInt(9)));
             }
-            
+
             ps.close();
             rs.close();
             conn.close();
-            
-            } catch (SQLException ex) {
-                Logger.getLogger(FXMLKlientController.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLKlientController.class.getName()).log(Level.SEVERE, null, ex);
         }
-            
-            
-            
-            
-            
-            
-            
-            FilteredList<Klient> filteredKlient = new FilteredList <>(lvKlienci.getItems(), e->true);
-            tfWyszukaj.setOnKeyReleased(e->{
-                tfWyszukaj.textProperty().addListener((observableValue, oldValue, newValue) ->{
-                    filteredKlient.setPredicate((Predicate<? super Klient>) k->{
-                        if (newValue==null || newValue.isEmpty()){
-                            return true;
-                        }
-                        String lcFilter = newValue.toLowerCase();
-                        if (k.getImie_klienta().toLowerCase().contains(lcFilter) || k.getNazwisko_klienta().toLowerCase().contains(lcFilter) || k.getNumer_karty().toString().contains(lcFilter)){
-                            return true;
-                        }
-                        return false;
-                        });
-                    });
-                SortedList<Klient> sortedKlient = new SortedList<>(filteredKlient);
-                lvKlienci.setItems(sortedKlient);
+
+        FilteredList<Klient> filteredKlient = new FilteredList<>(lvKlienci.getItems(), e -> true);
+        tfWyszukaj.setOnKeyReleased(e -> {
+            tfWyszukaj.textProperty().addListener((observableValue, oldValue, newValue) -> {
+                filteredKlient.setPredicate((Predicate<? super Klient>) k -> {
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+                    String lcFilter = newValue.toLowerCase();
+                    if (k.getImie_klienta().toLowerCase().contains(lcFilter) || k.getNazwisko_klienta().toLowerCase().contains(lcFilter) || k.getNumer_karty().toString().contains(lcFilter)) {
+                        return true;
+                    }
+                    return false;
+                });
             });
-            
-            
-        
-        
-              
-    }   
-    
-    
-    
+            SortedList<Klient> sortedKlient = new SortedList<>(filteredKlient);
+            lvKlienci.setItems(sortedKlient);
+        });
+
+    }
 
     @FXML
     private void dodajKlienta(ActionEvent event) throws IOException {
         Stage stage;
         Parent root;
-        
+
         stage = new Stage();
         root = FXMLLoader.load(getClass().getResource("FXMLDodajKlienta.fxml"));
         stage.setScene(new Scene(root));
@@ -153,10 +137,9 @@ public class FXMLKlientController implements Initializable {
 
     @FXML
     private void edytuj(ActionEvent event) throws IOException {
-        if (lvKlienci.getSelectionModel().getSelectedItem() != null){
-        
-            idKlienta = lvKlienci.getSelectionModel().getSelectedItem().getId_klienta();
+        if (lvKlienci.getSelectionModel().getSelectedItem() != null) {
 
+            idKlienta = lvKlienci.getSelectionModel().getSelectedItem().getId_klienta();
 
             Stage stage;
             Parent root;
@@ -169,18 +152,37 @@ public class FXMLKlientController implements Initializable {
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.initOwner(bNowyKlient.getScene().getWindow());
             stage.showAndWait();
-        
+
         }
     }
 
     @FXML
     private void usun(ActionEvent event) {
         //okno dialogowe TAK/NIE czy usunac klienta
-        
-         if (lvKlienci.getSelectionModel().getSelectedItem() != null){
-             
-         }
-        
+
+        if (lvKlienci.getSelectionModel().getSelectedItem() != null) {
+
+            JFXButton bCancel = new JFXButton("Nie");
+            JFXButton bOkay = new JFXButton("Tak");
+            AlertMaker.showMaterialDialog(spMain, apMain, Arrays.asList(bOkay, bCancel), "Usuwanie klienta.", "Czy na pewno chcesz usunąć zaznaczonego klienta?");
+
+            bOkay.setOnAction((ActionEvent event1) -> {
+                try {
+                    Connection conn = DBConnection.Connect();
+                    int id = lvKlienci.getSelectionModel().getSelectedItem().getId_klienta();
+                    lvKlienci.getItems().remove(lvKlienci.getSelectionModel().getSelectedIndex());
+                    
+                    conn.createStatement().executeUpdate("DELETE FROM klient WHERE id_klienta=" + id + ";");
+
+                    conn.close();
+                } catch (Exception exp) {
+                    //nieobsłużone
+                    //System.out.println(exp);
+                }
+            });
+
+        }
+
     }
-    
+
 }
